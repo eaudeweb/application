6359637906c5e36f380b8a38244e8a6d063b2530
Andra Necula <30728373+anecula@users.noreply.github.com>
2018-01-12 13:46:41 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 if [[ -z $NODE_ENV ]]; then
@@ -30,24 +31,82 @@ else
 	SMTP_PASSWORD=password
 fi
 
-if [[ -e /opt/lamunca-management/config/crypto_secret ]]; then
-	CRYPTO_SECRET=$(cat /opt/lamunca-management/config/crypto_secret)
+if [[ -e /opt/timeoff-management/config/crypto_secret ]]; then
+	CRYPTO_SECRET=$(cat /opt/timeoff-management/config/crypto_secret)
 else
-	echo -n $(tr -dc A-Za-z0-9_\#\(\)\!: < /dev/urandom | head -c 40 | xargs) > /opt/lamunca-management/config/crypto_secret
-	CRYPTO_SECRET=$(cat /opt/lamunca-management/config/crypto_secret)
+	echo -n $(tr -dc A-Za-z0-9_\#\(\)\!: < /dev/urandom | head -c 40 | xargs) > /opt/timeoff-management/config/crypto_secret
+	CRYPTO_SECRET=$(cat /opt/timeoff-management/config/crypto_secret)
 fi
 
 if [[ -z $APP_URL ]]; then
-	APP_URL=https://lamunca.eaudeweb.ro
+	APP_URL=http://app.timeoff.management
 fi
 
 if [[ -z $PROMOTION_URL ]]; then
-	PROMOTION_URL=https://lamunca.eaudeweb.ro
+	PROMOTION_URL=http://timeoff.management
 fi
 
 if [[ -z $ALLOW_ACCOUNTS_CREATION ]]; then
 	ALLOW_ACCOUNTS_CREATION=true
 fi
 
+cat > /opt/timeoff-management/config/app.json << EOF
+{
+  "allow_create_new_accounts" : $ALLOW_ACCOUNTS_CREATION,
+  "send_emails"              : $SEND_MAILS,
+  "application_sender_email" : "$SENDER_MAIL",
+  "email_transporter" : {
+    "host" : "$SMTP_HOST",
+    "port" : $SMTP_PORT,
+    "auth" : {
+      "user" : "$SMTP_USER",
+      "pass" : "$SMTP_PASSWORD"
+    }
+  },
+  "crypto_secret" : "$CRYPTO_SECRET",
+  "application_domain" : "$APP_URL",
+  "promotion_website_domain" : "$PROMOTION_URL"
+}
+EOF
 
-npm start
+cat > /opt/timeoff-management/config/db.json << EOF
+{
+  "development": {
+    "dialect": "sqlite",
+    "storage": "./db.development.sqlite"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+EOF
+
+if [[ -n $MYSQL_HOST && -n $MYSQL_USER && -n $MYSQL_PASSWORD ]]; then
+	if [[ -z $MYSQL_DATABASE ]]; then
+		MYSQL_DATABASE="timeoffmanagement"
+	fi
+	cat >> /opt/timeoff-management/config/db.json << EOF
+  "production": {
+    "username": "$MYSQL_USER",
+    "password": "$MYSQL_PASSWORD",
+    "database": "$MYSQL_DATABASE",
+    "host": "$MYSQL_HOST",
+    "dialect": "mysql"
+  }
+}
+EOF
+else
+	cat >> /opt/timeoff-management/config/db.json << EOF
+  "production": {
+    "dialect": "sqlite",
+    "storage": "./db.production.sqlite"
+  }
+}
+EOF
+fi
+
+npm run-script db-update
+
